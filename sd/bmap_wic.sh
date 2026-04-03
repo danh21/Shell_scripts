92e4d3a45e80b741606c526baa96c5bd2b2ee25d
Danh Phan <phandanh2112@gmail.com>
2026-04-03 11:31:50 +0700

    v1.1.0: modify filename to wildcard format

--- sd/bmap_wic.sh
+++ sd/bmap_wic.sh
@@ -4,23 +4,29 @@ sd="/dev/sdb"
 device=$1
 
 if [ "$device" = "g3s" ]; then
-    bmap="core-image-minimal-smarc-rzg3s*.wic.bmap"
-	gz="core-image-minimal-smarc-rzg3s*.wic.gz"
+    bmap="core*rzg3s*.wic.bmap"
+	gz="core*rzg3s*.wic.gz"
 elif [ "$device" = "v2h" ]; then
-    bmap="core-image-minimal-rzv2h-evk-ver1*.wic.bmap"
-	gz="core-image-minimal-rzv2h-evk-ver1*.wic.gz"
+    bmap="core*rzv2h*.wic.bmap"
+	gz="core*rzv2h*.wic.gz"
 elif [ "$device" = "g3e" ]; then
-    bmap="core-image-minimal-smarc-rzg3e*.wic.bmap"
-	gz="core-image-minimal-smarc-rzg3e*.wic.gz"
+    bmap="core-image-*-smarc-rzg3e*.wic.bmap"
+	gz="core-image-*-smarc-rzg3e*.wic.gz"
 elif [ "$device" = "v2n" ]; then
-    bmap="core-image-minimal-rzv2n-evk*.wic.bmap"
-	gz="core-image-minimal-rzv2n-evk*.wic.gz"
+    bmap="core-image-*-rzv2n*.wic.bmap"
+	gz="core-image-*-rzv2n*.wic.gz"
 elif [ "$device" = "g2l" ]; then
-    bmap="core-image-minimal-smarc-rzg2l*.wic.bmap"
-	gz="core-image-minimal-smarc-rzg2l*.wic.gz"
+    bmap="core*rzg2l.*.wic.bmap"
+	gz="core*rzg2l.*.wic.gz"
 elif [ "$device" = "g2lc" ]; then
-    bmap="core-image-minimal-smarc-rzg2lc*.wic.bmap"
-	gz="core-image-minimal-smarc-rzg2lc*.wic.gz"
+    bmap="core-image-*-smarc-rzg2lc*.wic.bmap"
+	gz="core-image-*-smarc-rzg2lc*.wic.gz"
+elif [ "$device" = "g2ul" ]; then
+    bmap="core-image-*-smarc-rzg2ul*.wic.bmap"
+	gz="core-image-*-smarc-rzg2ul*.wic.gz"
+elif [ "$device" = "g3l" ]; then
+    bmap="core-image-*-smarc-rzg3l*.wic.bmap"
+	gz="core-image-*-smarc-rzg3l*.wic.gz"
 else
 	echo "nothing"
 fi
